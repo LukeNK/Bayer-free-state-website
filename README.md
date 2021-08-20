10f4f58da93026db06c39ab56a12101b0882cb30
LukeNK <64694123+LukeNk@users.noreply.github.com>
2021-08-20 16:37:28 -0700

    Basic setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# Bayer Free State government website
+
+# For developer
+- src: every code is in this folder
+    - page: html files
+    - public: files will be serve on `/public`
+        - comp: components will be render 
+        - scrip: contains client js files
+        - style: css files
+    - `index.js`: entry point for server
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.send('hello world');
+});
+
+app.post('/API/*', (req, res) => {});
+
+app.use(express.static('public'));
+
+app.listen(process.env.listenPort, () => {
+    console.warn('Listening at port ' + process.env.listenPort);
 })
