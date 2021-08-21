ac558f93a9f92986e4d5f684d61ea0787b91d1cf
LukeNK <64694123+LukeNk@users.noreply.github.com>
2021-08-21 13:09:39 -0700

    Template engine draft

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 # For developer
 - src: every code is in this folder
     - page: html files
-    - public: files will be serve on `/public`
+    - public: files will be served on `/public`
         - comp: components will be render 
         - scrip: contains client js files
         - style: css files
--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 require('dotenv').config();
 const express = require('express');
+const fs = require('fs');
 var app = express();
 
+function serveFile(fName, callback) {
+    fs.readFile(fName, 'utf8', (err, data) => {
+        if (err) { console.warn(err); return }
+        let req = data.match(/{{(.*?)}}/g);
+        let count = req.length;
+        for (let name of req) {
+            name = name.substr(2, name.length - 4);
+            fs.readFile('./public/comp/' + name, 'utf8', (err, dat) => {
+                if (err) { console.warn(err); }
+                data = data.replace(/{{(.*?)}}/g, dat);
+                count--;
+                if (!count) callback(data);
+            });
+        }
+    })
+}
+
 app.get('/', (req, res) => {
-    res.send('hello world');
+    serveFile('./page/doc/constitution.html', (data) => {
+        res.send(data);
+    })
 });
 
 app.post('/API/*', (req, res) => {});
