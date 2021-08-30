963a244d3653c71f8d03ea90ebd3e2c337b19241
oofiedev <tdkmike7406@gmail.com>
2021-08-30 23:45:28 +0700

    Added Side Menu to Navbar

--- src/root/public/script.js
+++ src/root/public/script.js
@@ -18,3 +18,11 @@ function setCookie(cname, cvalue, exdays) {
     let expires = 'expires=' + d.toUTCString();
     document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
 }
+
+function openNav() {
+    document.getElementById('sideMenu').style.width = '250px';
+}
+
+function closeNav() {
+    document.getElementById('sideMenu').style.width = '0';
+}
