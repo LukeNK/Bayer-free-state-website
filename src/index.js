ec3a32f7a3f2d6bb3fe4eb509c8263e1bb40df3a
oofie <tdkmike7406@gmail.com>
2021-12-13 20:47:11 +0700

    Fixed deprecated substr to substring

--- src/index.js
+++ src/index.js
@@ -15,96 +15,98 @@ let IP_LIST = {};
  * @param {Function} callback Callback. Params: (data, err)
  */
 function serveFile(fName, callback) {
-    fs.readFile(fName, 'utf8', (err, data) => {
-        if (err) return callback(null, err);
-        let req = data.match(/{{(.*?)}}/g);
+	fs.readFile(fName, 'utf8', (err, data) => {
+		if (err) return callback(null, err);
+		let req = data.match(/{{(.*?)}}/g);
 
-        if (!req) return callback(data);
-        let count = req.length;
+		if (!req) return callback(data);
+		let count = req.length;
 
-        for (let name of req) {
-            name = name.substr(2, name.length - 4);
-            fs.readFile('./root/comp/' + name, 'utf8', (err, dat) => {
-                count--;
-                data = data.replace(RegExp(`{{${name}}}`, 'g'), dat || '');
-                if (err) return console.warn(err);
-                if (!count) callback(data);
-            });
-        }
-    });
+		for (let name of req) {
+			name = name.substring(2, name.length - 2);
+			fs.readFile('./root/comp/' + name, 'utf8', (err, dat) => {
+				count--;
+				data = data.replace(RegExp(`{{${name}}}`, 'g'), dat || '');
+				if (err) return console.warn(err);
+				if (!count) callback(data);
+			});
+		}
+	});
 }
 
 router.use((req, res, next) => {
-    if (req.url === '/403') return next();
+	if (req.url === '/403') return next();
 
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    if (!IP_LIST[ip]) IP_LIST[ip] = 0;
+	const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+	if (!IP_LIST[ip]) IP_LIST[ip] = 0;
 
-    if (IP_LIST[ip] <= config.rateLimit.maxCount) {
-        IP_LIST[ip]++;
-        console.log(IP_LIST[ip]);
-        console.log(`IP: ${ip}, URL: ${req.url}`);
-        // setTimeout(() => {
-        //     IP_LIST[ip]--;
-        // }, config.rateLimit.timeout);
-    }
-    else return res.redirect(403, '/403');
+	if (IP_LIST[ip] <= config.rateLimit.maxCount) {
+		IP_LIST[ip]++;
+		console.log(IP_LIST[ip]);
+		console.log(`IP: ${ip}, URL: ${req.url}`);
+		// setTimeout(() => {
+		//     IP_LIST[ip]--;
+		// }, config.rateLimit.timeout);
+	} else return res.redirect(403, '/403');
 
-    next();
+	next();
 });
 
 app.use(router);
 
 // clear bans
 setInterval(() => {
-    for (const [ key, _ ] of Object.entries(IP_LIST)) {
-        IP_LIST[key] = 0;
-    }
+	for (const [ key, _ ] of Object.entries(IP_LIST)) {
+		IP_LIST[key] = 0;
+	}
 }, config.rateLimit.timeout);
 
 app.get('/', (req, res) => {
-    serveFile('./root/pages/index.html', (data) => {
-        res.send(data);
-    });
+	serveFile('./root/pages/index.html', (data) => {
+		res.send(data);
+	});
 });
 
 app.get('/link/:name', (req, res) => {
-    try {
-        res.redirect(config.links[req.params.name]);
-    } catch (err) {
-        res.redirect('/404');
-    }
+	try {
+		res.redirect(config.links[req.params.name]);
+	} catch (err) {
+		res.redirect('/404');
+	}
 });
 
-
 app.post('/API/statute', (req, res) => {
-    let inData = ''
-    req.on('data', (c) => { inData += c })
-    req.on('end', () => {
-        https.get(path.join(config.links.statueBase, data), (res2) => {
-            let outData = '';
-            res2.on('data', (c) => { outData += c });
-            res2.on('end', () => {
-                res.send(outData);
-            });
-            res2.on('error', () => {
-                res.sendStatus(404);
-                res.send('')
-            })
-        })
-    })
+	let inData = '';
+	req.on('data', (c) => {
+		inData += c;
+	});
+	req.on('end', () => {
+		https.get(path.join(config.links.statueBase, data), (res2) => {
+			let outData = '';
+			res2.on('data', (c) => {
+				outData += c;
+			});
+			res2.on('end', () => {
+				res.send(outData);
+			});
+			res2.on('error', () => {
+				res.sendStatus(404);
+				res.send('');
+			});
+		});
+	});
 });
 
 app.use('/public', express.static('root/public'));
 
 app.get('*', (req, res) => {
-    // pages (no .html)
-    serveFile('./root/pages/' + req.url + '.html', (data, err) => {
-        if (err && req.params.name !== '404') return res.redirect('/404');
-        res.send(data);
-    });
+	// pages (no .html)
+	serveFile('./root/pages/' + req.url + '.html', (data, err) => {
+		if (err && req.params.name !== '404') return res.redirect('/404');
+		res.send(data);
+	});
 });
 
 app.listen(process.env.PORT, () => {
-    console.warn(`Listening at port ${process.env.PORT}.`);
+	console.warn(`Listening at port ${process.env.PORT}.`);
 });
--- src/root/public/statute.js
+++ src/root/public/statute.js
@@ -1,36 +1,36 @@
 let statute = {
-    botFile: undefined,
-    parseCode(code) {
-        let l = code.length;
-        code = code.split('_');
-        if (l !== 12 || code.length !== 3) return null;
-        return {
-            type: code[0],
-            date: code[1],
-            index: code[2]
-        }
-    },
-    getBot(callback) {
-        if (this.botFile) callback(...this.botFile);  // If already downloaded
-        getData('/API/statute', 'bot.json', data => {
-            this.botFile = JSON.parse(data);
-            callback(...this.botFile);
-        })
-    },
-    getAct(inp, callback) {
-        // inp can be reference code or path
-        this.getBot(data => { 
-            let t = this.parseCode(inp);
-            let path = '';
+	botFile: undefined,
+	parseCode(code) {
+		let l = code.length;
+		code = code.split('_');
+		if (l !== 12 || code.length !== 3) return null;
+		return {
+			type: code[0],
+			date: code[1],
+			index: code[2]
+		};
+	},
+	getBot(callback) {
+		if (this.botFile) callback(...this.botFile); // If already downloaded
+		getData('/API/statute', 'bot.json', (data) => {
+			this.botFile = JSON.parse(data);
+			callback(...this.botFile);
+		});
+	},
+	getAct(inp, callback) {
+		// inp can be reference code or path
+		this.getBot((data) => {
+			let t = this.parseCode(inp);
+			let path = '';
 
-            if (t) {
-                path = data[inp];
-            } else for (cur of data) if (cur.path == inp) path = cur.path;
-                    
-            getData('/API/statute/', path, (act) => {
-                act = JSON.parse(act);
-                callback(act);
-            })
-        });
-    }
-}
+			if (t) {
+				path = data[inp];
+			} else for (cur of data) if (cur.path == inp) path = cur.path;
+
+			getData('/API/statute/', path, (act) => {
+				act = JSON.parse(act);
+				callback(act);
+			});
+		});
+	}
+};
